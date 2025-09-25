ebf236741809d9a146fbac08aa7cfe07a03557e8
Your GitHub Username <your-email@example.com>
2025-09-25 16:10:25 -0700

    hhjh

--- Ashesi Premier League/PlayerSelection.java
+++ Ashesi Premier League/PlayerSelection.java
@@ -21,12 +21,29 @@ public class PlayerSelection {
         int jerseyNumber = input.nextInt();
 
         // Display details (Sample Output)
-        System.out.println("\n--- Player Details ---");
-        System.out.println("Player Name – " + playerName);
-        System.out.println("Age – " + playerAge);
-        System.out.println("Height – " + playerHeight + "m");
-        System.out.println("Weight – " + playerWeight + "lbs");
-        System.out.println("Jersey Number – " + jerseyNumber);
+        System.out.println("\n Player Details ");
+        System.out.println("Player Name" + playerName);
+        System.out.println("Age " + playerAge);
+        System.out.println("Height " + playerHeight + "m");
+        System.out.println("Weight " + playerWeight + "lbs");
+        System.out.println("Jersey Number" + jerseyNumber);
+
+        // Task 2
+        final double Pound = 0.45359237;
+        final int meter = 100;
+
+        double weightInKg = playerWeight * Pound;
+        double heightInCm = playerHeight * meter;
+
+        System.out.println("\n Converted Values");
+        System.out.println("Player Name" + name);
+        System.out.println("Age" + age);
+        System.out.println("Height in centimeters " + (int)heightCm + "cm");
+        System.out.println("Weight in kilograms" + roundedWeight + "kg");
+        System.out.println("Jersey Number " + jerseyNumber);
+
+
+
 
         input.close(); // Close at the very end
     }
